dfc304b0f86162905a5bfb5972150f99f3177baf
Tommy Truong <ttruong@campaignmonitor.com>
2021-06-29 15:39:45 +1000

    Add unimplemented request util.

---
+++ frontend/src/utils/fetch/requests.js
@@ -0,0 +1,11 @@
+const generateGetRequest = "not implemented";
+const generatePutRequest = "not implemented";
+const generatePostRequest = "not implemented";
+const generateDeleteRequest = "not implemented";
+
+export {
+    generateGetRequest, 
+    generatePutRequest, 
+    generatePostRequest, 
+    generateDeleteRequest
+};
